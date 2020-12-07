c4671a6db2ad9a747ebed0aa9ff809f3aed9759a
maozhongyu <mao.zhongyu@qianka.com>
2020-12-07 23:04:17 +0800

    获得redis 连接

--- project/code/nginx/application/access.lua
+++ project/code/nginx/application/access.lua
@@ -3,3 +3,24 @@
 --- Created by mac.
 --- DateTime: 2020/12/7 下午9:56
 ---
+local ngx_re_split = require("ngx.re").split
+local redis_cluster = require "rediscluster"
+local ip_addr = ngx.shared.redis_cluster_addr:get('redis-addr')
+local ip_addr_table = ngx_re_split(ip_addr, ",")
+local redis_addr = {}
+for key, value in ipairs(ip_addr_table) do
+    local ip_addr = ngx_re_split(value, ":")
+    redis_addr[key] = { ip = ip_addr[1], port = ip_addr[2] }
+end
+local config = {
+    name = "testCluster", --rediscluster name
+    serv_list = redis_addr,
+    keepalive_timeout = 60000, --redis connection pool idle timeout
+    keepalive_cons = 1000, --redis connection pool size
+    connection_timout = 1000, --timeout while connecting
+    max_redirection = 5, --maximum retry attempts for redirection
+
+}
+local red_c = redis_cluster:new(config)
+
+ngx.ctx.redisObject = red_c
